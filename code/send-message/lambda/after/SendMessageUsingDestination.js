f32452849dbe705fb5c522d926bafbf06cfe0bde
Pillai <pisindhu@amazon.com>
2022-04-08 10:12:21 -0700

    udpated function

--- code/send-message/lambda/after/SendMessageUsingDestination.js
+++ code/send-message/lambda/after/SendMessageUsingDestination.js
@@ -1,5 +1,5 @@
 //Note: There is no mention of SQS within the code, yet it is integrated with SQS using lambda destinations
-exports.handler = async function(event) {
+exports.handler = async (event) =>  {
 
     //some business logic
 
