604f5b8b075a211f91d201e0e21970c81e2f86f9
fpengzha <fpengzha@amazon.com>
2023-05-26 11:19:43 +0800

    Refine the code a bit

--- implementation/orchestration-to-choreography/lib/orchestration.ts
+++ implementation/orchestration-to-choreography/lib/orchestration.ts
@@ -1,13 +1,13 @@
 import {
-    Stack,
-    StackProps,
-    RemovalPolicy,
-    aws_lambda as lambda,
-    aws_iam as iam,
-    aws_dynamodb as dynamodb,
-    aws_stepfunctions as sfn,
-    aws_stepfunctions_tasks as tasks
-  } from 'aws-cdk-lib';
+  Stack,
+  StackProps,
+  RemovalPolicy,
+  aws_lambda as lambda,
+  aws_iam as iam,
+  aws_dynamodb as dynamodb,
+  aws_stepfunctions as sfn,
+  aws_stepfunctions_tasks as tasks
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NagSuppressions } from 'cdk-nag'
 
@@ -35,96 +35,96 @@ export class OrchestrationStack extends Stack {
         'arn:aws:logs:' + this.region + ':' + this.account + ':log-group:/aws/lambda/BankRecipientPremium:*',
         'arn:aws:logs:' + this.region + ':' + this.account + ':log-group:/aws/lambda/BankRecipientUniversal:*'
       ],
-  }))
+    }))
 
-  const stepRole = new iam.Role(this, 'stepRole', {
-    path: "/service-role/",
-    assumedBy: new iam.ServicePrincipal('states.amazonaws.com'),
-  });
-  
-  const policy = new iam.PolicyStatement({
-    actions: [
-      "dynamodb:Get*",
-      "lambda:InvokeFunction"
-    ],
-    resources: [
-      "arn:aws:dynamodb:*:*:table/*",
-      "arn:aws:lambda:*:*:function:*"
-    ], 
-    effect: iam.Effect.ALLOW
-  });
-  
-  const customPolicy = new iam.Policy(this, 'customPolicy', {
-    statements: [policy]
-  });
-  
-  customPolicy.attachToRole(stepRole);  
+    const stepRole = new iam.Role(this, 'stepRole', {
+      path: "/service-role/",
+      assumedBy: new iam.ServicePrincipal('states.amazonaws.com'),
+    });
 
-  // Create the Lambda functions
-  const getCreditScoreFn = new lambda.Function(this, 'getCreditScore', {
-    functionName: `GetCreditScore`,
-    runtime: lambda.Runtime.NODEJS_18_X,
-    code: lambda.Code.fromAsset('lambda/orchestration'),
-    handler: 'index.handler',
-    role: orchestrationRole,
-  });
+    const policy = new iam.PolicyStatement({
+      actions: [
+        "dynamodb:Get*",
+        "lambda:InvokeFunction"
+      ],
+      resources: [
+        "arn:aws:dynamodb:*:*:table/*",
+        "arn:aws:lambda:*:*:function:*"
+      ],
+      effect: iam.Effect.ALLOW
+    });
 
-  const bankRecipientPawnshopFn = new lambda.Function(this, 'BankRecipientPawnshop', {
-    environment: {
+    const customPolicy = new iam.Policy(this, 'customPolicy', {
+      statements: [policy]
+    });
+
+    customPolicy.attachToRole(stepRole);
+
+    // Create the Lambda functions
+    const getCreditScoreFn = new lambda.Function(this, 'getCreditScore', {
+      functionName: `GetCreditScore`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda/orchestration'),
+      handler: 'index.handler',
+      role: orchestrationRole,
+    });
+
+    const bankRecipientPawnshopFn = new lambda.Function(this, 'BankRecipientPawnshop', {
+      environment: {
         max_loan_amount: "500000",
         min_credit_score: "400",
         bank_id: "PawnShop",
         base_rate: "5"
-    },
-    functionName: `BankRecipientPawnshop`,
-    runtime: lambda.Runtime.NODEJS_18_X,
-    code: lambda.Code.fromAsset('lambda/orchestration'),
-    handler: 'RecipientBank.handler',
-    role: orchestrationRole
-  });
+      },
+      functionName: `BankRecipientPawnshop`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda/orchestration'),
+      handler: 'RecipientBank.handler',
+      role: orchestrationRole
+    });
 
-  const bankRecipientPremiumFn = new lambda.Function(this, 'BankRecipientPremium', {
-    environment: {
+    const bankRecipientPremiumFn = new lambda.Function(this, 'BankRecipientPremium', {
+      environment: {
         max_loan_amount: "900000",
         min_credit_score: "600",
         bank_id: "Premium",
         base_rate: "3"
-    },
-    functionName: `BankRecipientPremium`,
-    runtime: lambda.Runtime.NODEJS_18_X,
-    code: lambda.Code.fromAsset('lambda/orchestration'),
-    handler: 'RecipientBank.handler',
-    role: orchestrationRole
-  });
+      },
+      functionName: `BankRecipientPremium`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda/orchestration'),
+      handler: 'RecipientBank.handler',
+      role: orchestrationRole
+    });
 
-  const bankRecipientUniversalFn = new lambda.Function(this, 'BankRecipientUniversal', {
-    environment: {
-          max_loan_amount: "700000",
-          min_credit_score: "500",
-          bank_id: "Universal",
-          base_rate: "4"
-    },
-    functionName: `BankRecipientUniversal`,
-    runtime: lambda.Runtime.NODEJS_18_X,
-    code: lambda.Code.fromAsset('lambda/orchestration'),
-    handler: 'RecipientBank.handler',
-    role: orchestrationRole
-  });
+    const bankRecipientUniversalFn = new lambda.Function(this, 'BankRecipientUniversal', {
+      environment: {
+        max_loan_amount: "700000",
+        min_credit_score: "500",
+        bank_id: "Universal",
+        base_rate: "4"
+      },
+      functionName: `BankRecipientUniversal`,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda/orchestration'),
+      handler: 'RecipientBank.handler',
+      role: orchestrationRole
+    });
 
-  // Create DynamoDB table
-  const bankTable = new dynamodb.Table(this, 'LoanBrokerBanks', {
-    tableName: 'LoanBrokerBanks',
-    partitionKey: {
-      name: 'Type',
-      type: dynamodb.AttributeType.STRING
-    },
-    removalPolicy: RemovalPolicy.DESTROY
-  });
+    // Create DynamoDB table
+    const bankTable = new dynamodb.Table(this, 'LoanBrokerBanks', {
+      tableName: 'LoanBrokerBanks',
+      partitionKey: {
+        name: 'Type',
+        type: dynamodb.AttributeType.STRING
+      },
+      removalPolicy: RemovalPolicy.DESTROY
+    });
 
-  // Create the Step Functions state machine
-  const StepFunctionsStateMachine = new sfn.CfnStateMachine(this, 'StepFunctionsStateMachine', {
-    stateMachineName: "LoanBroker",
-    definitionString: `
+    // Create the Step Functions state machine
+    const StepFunctionsStateMachine = new sfn.CfnStateMachine(this, 'StepFunctionsStateMachine', {
+      stateMachineName: "LoanBroker",
+      definitionString: `
 {
 "Comment": "A description of my state machine",
 "StartAt": "Get Credit Score",
@@ -230,22 +230,12 @@ export class OrchestrationStack extends Stack {
 }
 }
 `,
-    roleArn: stepRole.roleArn,
-    stateMachineType: "STANDARD",
-    loggingConfiguration: {
+      roleArn: stepRole.roleArn,
+      stateMachineType: "STANDARD",
+      loggingConfiguration: {
         includeExecutionData: false,
         level: "OFF"
-    }
+      }
     });
-  
   }
 }
-
-
-
-
-
-
- 
-
-    
